88c7ff514fa3a05fe24235a0558de11bbb0a2e5e
Ravendra kumar <111950764+ravendrakum@users.noreply.github.com>
2023-12-30 21:27:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // This is the boilerplate code given for you
 // You can modify this code
-// Product data
 const products = [
   { id: 1, name: "Product 1", price: 10 },
   { id: 2, name: "Product 2", price: 20 },
@@ -11,10 +10,17 @@ const products = [
 
 // DOM elements
 const productList = document.getElementById("product-list");
+const cartList = document.getElementById("cart-list");
+
+// Cart data stored in session storage
+let cart = JSON.parse(sessionStorage.getItem("cart")) || [];
+
+// Simulate asynchronous behavior with setTimeout
+const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
 // Render product list
 function renderProducts() {
-  products.forEach((product) => {
+  products.forEach(async (product) => {
     const li = document.createElement("li");
     li.innerHTML = `${product.name} - $${product.price} <button class="add-to-cart-btn" data-id="${product.id}">Add to Cart</button>`;
     productList.appendChild(li);
@@ -22,17 +28,60 @@ function renderProducts() {
 }
 
 // Render cart list
-function renderCart() {}
+async function renderCart() {
+  await delay(0); // Simulate asynchronous behavior
+
+  cartList.innerHTML = ""; // Clear the cart list before rendering
+
+  cart.forEach(async (cartItem) => {
+    const li = document.createElement("li");
+    li.innerHTML = `${cartItem.name} - $${cartItem.price} <button class="remove-from-cart-btn" data-id="${cartItem.id}">Remove</button>`;
+    cartList.appendChild(li);
+  });
+}
 
 // Add item to cart
-function addToCart(productId) {}
+async function addToCart(productId) {
+  const productToAdd = products.find((product) => product.id === productId);
+
+  if (productToAdd) {
+    cart.push({ id: productToAdd.id, name: productToAdd.name, price: productToAdd.price });
+    await updateCart();
+  }
+}
 
 // Remove item from cart
-function removeFromCart(productId) {}
+async function removeFromCart(productId) {
+  cart = cart.filter((cartItem) => cartItem.id !== productId);
+  await updateCart();
+}
 
 // Clear cart
-function clearCart() {}
+async function clearCart() {
+  cart = [];
+  await updateCart();
+}
+
+// Update cart and session storage
+async function updateCart() {
+  await renderCart();
+  sessionStorage.setItem("cart", JSON.stringify(cart));
+}
+
+// Event delegation for "Add to Cart" and "Remove" buttons
+document.addEventListener("click", async (event) => {
+  if (event.target.classList.contains("add-to-cart-btn")) {
+    const productId = parseInt(event.target.dataset.id);
+    await addToCart(productId);
+  }
+
+  if (event.target.classList.contains("remove-from-cart-btn")) {
+    const productId = parseInt(event.target.dataset.id);
+    await removeFromCart(productId);
+  }
+});
 
 // Initial render
 renderProducts();
 renderCart();
+
